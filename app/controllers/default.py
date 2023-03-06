5eaa1cd6c22394fd52d6707847229dedcc069af8
Luzia Gabriela Abreu da Silva Santos <73603007+Luzia-Silva@users.noreply.github.com>
2023-03-05 22:16:41 -0300

    refactor pages and news implementations

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,17 @@
+from flask import render_template
 from app import app
+
+from app.models.forms import LoginForm
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    login = LoginForm()
+    if login.validate_on_submit():
+        print(login.username.data)
+    else :
+        print(login.form_errors)
+    return render_template("login.html", login=login)   
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ load_dotenv()
 import os 
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SECRET_KEY =  os.environ['SECRET_KEY']
